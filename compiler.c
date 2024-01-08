fd3ea447104332f0ad847ed5b80843ed0f66e55b
Tahlon <tahlonbrahic@gmail.com>
2024-01-08 16:14:55 -0600

    Start lexical anaylsis

--- compiler.c
+++ compiler.c
@@ -1,5 +1,18 @@
-#include <stdio.h>
+##include <stdio.h>
 
-void main {
+typedef enum {
+    TOKEN_INT,
+    TOKEN_PLUS,
+    TOKEN_STAR,
+    TOKEN_EOF,  // End of File token
+    TOKEN_INVALID
+} TokenType;
 
+typedef struct {
+    TokenType type;
+    int value; // Used for TOKEN_INT
+} Token;
+
+int main() {
+    return 0;
 }
