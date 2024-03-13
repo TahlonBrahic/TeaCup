64d11dab9555d077a34b171f1f718ac93263fec4
Tahlon Brahic <tahlonbrahic@gmail.com>
2024-03-13 09:53:42 -0500

    Corrected syntax error

--- compiler.c
+++ compiler.c
@@ -4,26 +4,35 @@
 #undef extern_
 #include <errno.h>
 
+// Reset state management
 static void init() {
     Line = 1;
     Putback = '\n';
 }
 
-char *tokstr[] = { "+", "-", "*", "/", "intlit" };
+char *tokstr[] = { "int", "+", "-", "*", "/", "eof", "invalid" };
 
 static void scanfile() {
     struct Token T;
     while (scan(&T)) {
-        printf("Token %s", tokstr[T.token]);
-        if (T.token == T_INTLIT)
-            printf(", value %d" T.intvalue);
+        printf("Token %s", tokstr[T.type]);
+        if (T.type == TOKEN_INT)
+            printf(", value %d", T.value);
         printf("\n");
     }
 }
 
-int main() {
+int main(int argc, char *argv[]) {
+    if (argc != 2)
+        usage(argv[0]);
 
     init();
+
+    if ((Infile = fopen(argv[1], "r")) == NULL) {
+        printf(stderr, "Unable to open %s: %s\n", argv[1], strerror(errno));
+        exit(1);
+    }
+
     scanfile();
     exit(0);
 };
