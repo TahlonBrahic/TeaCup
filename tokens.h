486b362e6b509fa902dd658d6a55051ac6095884
Tahlon Brahic <tahlonbrahic@gmail.com>
2024-03-13 11:11:33 -0500

    Add function prototypes to header file

--- tokens.h
+++ tokens.h
@@ -19,4 +19,8 @@ typedef struct {
     int value; // Used for TOKEN_INT
 } Token;
 
-int lexer(Token *t);
+int lexer(Token *t);static int scanint(int c);
+static int next(void);
+static void putback(int c);
+static int skip(void);
+int chrpos(const char *s, int c);
