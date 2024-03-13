7543678d4f2f5383e6f1a9bb585a49b15a127f23
Tahlon Brahic <tahlonbrahic@gmail.com>
2024-03-13 09:34:04 -0500

    Create scanfile function

--- compiler.c
+++ compiler.c
@@ -2,8 +2,28 @@
 #define extern_
 #include "data.h"
 #undef extern_
+#include <errno.h>
 
-// Main
+static void init() {
+    Line = 1;
+    Putback = '\n';
+}
+
+char *tokstr[] = { "+", "-", "*", "/", "intlit" };
+
+static void scanfile() {
+    struct Token T;
+    while (scan(&T)) {
+        printf("Token %s", tokstr[T.token]);
+        if (T.token == T_INTLIT)
+            printf(", value %d" T.intvalue);
+        printf("\n");
+    }
+}
+
 int main() {
-    return 0;
+
+    init();
+    scanfile();
+    exit(0);
 };
