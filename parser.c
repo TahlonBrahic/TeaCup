e1a242a433af26798f36a858599c9cf984201882
Tahlon Brahic <tahlonbrahic@gmail.com>
2024-03-14 10:38:55 -0500

    Create parser

--- parser.c
+++ parser.c
@@ -15,6 +15,7 @@ static struct ASTnode *primary(void) {
     }
 }
 
+// Map token types representing arithmetic operators to specific arithmetic operations in the AST.
 int arithop(int tok) {
     switch (tok) {
         case T_PLUS:
@@ -29,4 +30,89 @@ int arithop(int tok) {
             fprintf(stderr, "unknown token in arithop() on line %d\n", Line);
             exit(1);
     }
+}
+
+// Parse binary expressions and construct the corresponding AST.
+struct ASTnode *binexpr(void) {
+    struct ASTnode *n, *left, *right;
+    int nodetypel
+
+    left = primary();
+
+    if (Token.token == T_EOF)
+        return (left);
+    
+    nodetype = arithop(Token.token);
+
+    scan(&Token);
+
+    right = binexpr();
+
+    n = mkastnode(nodetype, left, right, 0);
+    return(n);
+}
+
+// List AST operators
+static char *ASTop[] = { "+", "-", "*", "/" };
+
+// Interpret the operators and return the final value from an AST.
+int interpretAST(struct ASTnode *n) {
+  int leftval, rightval;
+
+  // Get the left and right sub-tree values
+  if (n->left)
+    leftval = interpretAST(n->left);
+  if (n->right)
+    rightval = interpretAST(n->right);
+
+  // Debug: Print what we are about to do
+  if (n->op == A_INTLIT)
+    printf("int %d\n", n->intvalue);
+  else
+    printf("%d %s %d\n", leftval, ASTop[n->op], rightval);
+
+  switch (n->op) {
+    case A_ADD:
+      return (leftval + rightval);
+    case A_SUBTRACT:
+      return (leftval - rightval);
+    case A_MULTIPLY:
+      return (leftval * rightval);
+    case A_DIVIDE:
+      return (leftval / rightval);
+    case A_INTLIT:
+      return (n->intvalue);
+    default:
+      fprintf(stderr, "Unknown AST operator %d\n", n->op);
+      exit(1);
+  }
+}
+
+// Build and return an AST node.
+struct ASTnode *mkastnode(int op, struct ASTnode *left,
+			  struct ASTnode *right, int intvalue) {
+  struct ASTnode *n;
+
+  // Malloc a new ASTnode
+  n = (struct ASTnode *) malloc(sizeof(struct ASTnode));
+  if (n == NULL) {
+    fprintf(stderr, "Unable to malloc in mkastnode()\n");
+    exit(1);
+  }
+  // Copy in the field values and return it
+  n->op = op;
+  n->left = left;
+  n->right = right;
+  n->intvalue = intvalue;
+  return (n);
+}
+
+// Make an AST leaf node
+struct ASTnode *mkastleaf(int op, int intvalue) {
+  return (mkastnode(op, NULL, NULL, intvalue));
+}
+
+// Make a unary AST node: only one child
+struct ASTnode *mkastunary(int op, struct ASTnode *left, int intvalue) {
+  return (mkastnode(op, left, NULL, intvalue));
 }
