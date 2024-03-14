e0a5ee678ac2be9619ae25e0c654c19a4edff673
Tahlon Brahic <tahlonbrahic@gmail.com>
2024-03-14 09:55:02 -0500

    Add AST nodes and AST structure

--- tokens.h
+++ tokens.h
@@ -19,6 +19,29 @@ typedef struct {
     int value; // Used for TOKEN_INT
 } Token;
 
+// AST nodes
+enum {
+    A_ADD,
+    A_SUBSTRACT,
+    A_MULTIPLY,
+    A_DIVIDE,
+    A_INTLIT
+};
+
+// AST Structure
+struct ASTnode {
+    int op;
+    struct ASTnode *left;
+    struct ASTnode *right;
+    int intvalue;
+};
+
+struct ASTnode *mkastnode(int op, struct ASTnode *left, struct ASTnode *right, int intvalue);
+struct ASTnode *mkastleaf(int op, int intvalue);
+struct ASTnode *mkastunary(int op, struct ASTnode *left, int intvalue);
+struct ASTnode *binexpr(void);
+int interpretAST(struct ASTnode *n);
+
 int lexer(Token *t);static int scanint(int c);
 int next(void);
 void putback(int c);
